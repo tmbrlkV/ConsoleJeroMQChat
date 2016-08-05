f2a221509142a47be01ace37ad67ea28b7a8d469
Danila <gudkovdanila@gmail.com>
2016-08-05 12:11:59 +0300

    Logging implemented

--- src/main/java/com/server/chat/Server.java
+++ src/main/java/com/server/chat/Server.java
@@ -1,10 +1,14 @@
 package com.server.chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
 
 class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("start server");
+        logger.debug("Start chat server");
 
         try (ZMQ.Context context = ZMQ.context(1)) {
             ZMQ.Socket publisher = context.socket(ZMQ.PUB);
@@ -12,10 +16,10 @@ class Server {
             publisher.bind("tcp://*:10000");
             receiver.bind("tcp://*:10001");
 
-            System.out.println("listening for incoming connection...");
+            logger.debug("listening for incoming connection...");
             while (!Thread.currentThread().isInterrupted()) {
                 String message = receiver.recvStr();
-                System.out.println(message);
+                logger.debug(message);
                 publisher.send(message);
             }
         }
